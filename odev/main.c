21cfb2b879b3d862f63099d05defc87ea02f9a50
irmakkizil <irmakkizil1905@gmail.com>
2023-12-22 19:06:25 +0300

    Initial Commit

---
+++ odev/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  odev
+//
+//  Created by Irmak KIZIL on 22.12.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
